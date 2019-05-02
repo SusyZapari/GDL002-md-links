cbe55a73d0c88be6499c4376b0eab3d6b4fa65f7
Susana Zapari <susy.zapari@gmail.com>
2019-05-01 22:45:10 -0500

    + Unit tests, functions and dependencies added.

--- index.js
+++ index.js
@@ -1,3 +1,95 @@
-module.exports = () => {
-  // ...
+const fs = require("fs");
+const path = require("path");
+module.exports = {
+//funcion que verifica si el campo esta vacio
+pathInserted: function(pathFile){
+  if(pathFile == undefined){
+    console.log("false");
+    return false;
+  }
+  else{
+    return true;
+  } 
+},
+//función para saber si la ruta existe
+pathWorking: function(pathFile){
+  if(fs.existsSync(pathFile)){
+    console.log("true");
+    return true;
+  }
+    else{
+      console.log("false");
+      return false;
+    }
+    },
+//función para verificar si la ruta es un directorio
+pathDirectory: function(pathFile){
+  if(fs.statSync(pathFile).isDirectory()){
+    return true;
+  } else{
+    return false;
+  } 
+},
+
+//Función que revisa si es un .md
+pathMd: function(pathFile){
+  if (path.extname(pathFile)=== ".md") {
+    return true;
+  }
+  else {
+    return false;
+}
+},
+
+//Leer el archivo
+readingFile:function(pathFile) {
+let file =fs.readFileSync(pathFile, "utf-8");
+console.log(file);
+return true;
+},
+
+// Extraer links //
+//function urlify(data) {
+  // console.log(txt);
+  //const mdLinkRgEx = /\[(.+?)\]\(.+?\)/g;
+  //const mdLinkRgEx2 = /\[(.+?)\]\((.+?)\)/;
+
+  //let allLinks = data.match(mdLinkRgEx);
+  //var htmlLinks = [];
+  //for (var x in allLinks) {
+    //var grpdDta = mdLinkRgEx2.exec(allLinks[x]);
+    //var linkified = "<a href=\"" + grpdDta[2] + "\">" + grpdDta[1] + "<a>";
+    //htmlLinks.push(linkified);
+  //}
+  //return htmlLinks;
 };
+
+// // Resultado de leer archivo //
+// resultReadFile.then(
+//   (data)=> { // On Success
+//    console.log("resultado =");
+//    urlify(data).forEach(link => console.log(link));
+     
+//   },
+//   (err)=> { // On Error
+//       console.error(err);
+//   }
+// );
+
+
+// // Promesa, fx leer archivo //
+// module.exports = (filePath, options) => {
+//   return new Promise((resolve, reject)=> {
+//     // Leer el archivo
+//     fs.readFile(filePath, function(err, data){
+//       if (err){
+//         return reject(err);
+//       }
+//       resolve(data.toString());
+//     });
+//   });
+//   };
+
+
+
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,53 @@
-const mdLinks = require('../');
-
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+const mdLinks = require('../index.js');
+describe('pathInserted', () => {
+  it('should be a function', () => {
+    expect(typeof mdLinks.pathInserted).toBe("function");
   });
-
+  it('Should return false', () => {
+    expect(mdLinks.pathInserted()).toBe(false);
+  });
+  it('should return true', () => {
+    expect(mdLinks.pathInserted('./README.md')).toBe(true);
+      });
 });
+describe("pathWorking", () =>{
+  it("Should be false", () => {
+       expect(mdLinks.pathWorking("./README.txt")).toBe(false);
+   });
+   it("Should be true", () => {
+     expect(mdLinks.pathWorking("./README.md")).toBe(true);
+   });
+    });
+
+describe("pathDirectory", () =>{
+  it("Should be true", () => {
+   expect(mdLinks.pathDirectory("/Users/susydev/Documents/LABORATORIA/PROYECTOS/GDL002-md-links")).toBe(true);
+   });
+  it("Should be false", () => {
+   expect(mdLinks.pathDirectory("./README.md")).toBe(false);
+   });
+  });
+   
+   describe("pathMd", () => {
+    it("Should be true", () => {
+    expect(mdLinks.pathMd("./README.md")).toBe(true);
+    });describe("urlify", () =>{
+  it("Should be true", ()=> {
+    expect(mdLinks.urlify("./README.md")).toBe(htmlLinks);
+  });
+});
+    it("Should be false", () => {
+    expect(mdLinks.pathMd("./README.txt")).toBe(false);
+    });
+    });
+
+    describe("readingFile",()=> {
+      it("Should read the file", ()=>{
+        expect(mdLinks.readingFile("./prueba.md")).toBe(true);
+   });
+  }); 
+  // describe("urlify", () =>{
+  //   it("Should be true", ()=> {
+  //     expect(mdLinks.urlify("./README.md")).toBe(htmlLinks);
+  //   });
+  // });
